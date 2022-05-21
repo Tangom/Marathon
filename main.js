75ca0bbe2d9687c9bb3c7ad72af42737403d43da
Alexsandr Chigarev <majestik13@mail.ru>
2022-05-21 11:15:50 +0300

    Добавление фото

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import {allData} from './components/data.js';
 import {photo} from './components/AddPhoto.js';
 import {createPersons} from './components/Person.js';
 
-function create(item) {
+ function create(item) {
   item.forEach((item) => {
     createPersons(item.personList, item.list)
   });
