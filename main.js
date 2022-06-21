fc7c70ba4778af843a1de5894ff7d947b0c0f7c5
Alexsandr Chigarev <majestik13@mail.ru>
2022-06-21 15:47:43 +0300

    WebPack

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import {allData} from './components/data.js';
 import {photo} from './components/AddPhoto.js';
 import {createPersons} from './components/Person.js';
+import './styles/index.css';
 
 photo.setEventListeners();
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 // указали в какой файл будет собираться весь js и дали ему имя
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = { // module.exports — это синтаксис экспорта в Node.js
   entry: { main: './main.js' },
@@ -28,6 +29,23 @@ module.exports = { // module.exports — это синтаксис экспор�
         use: 'babel-loader',
         // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: /node_modules/
+      },
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+          'postcss-loader'
+        ]
       }]
   },
   plugins: [
@@ -35,6 +53,7 @@ module.exports = { // module.exports — это синтаксис экспор�
       template: './src/index.html'
     }),// путь к файлу index.html
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
   ]
 };
 
